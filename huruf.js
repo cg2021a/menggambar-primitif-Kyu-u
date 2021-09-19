f77c15d18997a5e1c765de53dbbcd476eef5ae69
Mel <57700613+Kyu-u@users.noreply.github.com>
2021-09-20 01:34:43 +0700

    Update huruf.js

--- huruf.js
+++ huruf.js
@@ -67,7 +67,7 @@ function main() {
     // Definisi fragment
     let fragmentShaderCode = `
       void main(){
-          gl_FragColor = vec4(0.3, 0.7, 1.0, 1.0);
+          gl_FragColor = vec4(1.0, 0.5, 0.8, 1.0);
       }
     `;
   
